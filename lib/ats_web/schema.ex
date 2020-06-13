21237801cf29b674a6194b1c7da7c0a894f132ea
Bastien Rivière <babathriviere@gmail.com>
2020-06-13 17:40:16 +0200

    Adds point field + some documentation

--- lib/ats_web/schema.ex
+++ lib/ats_web/schema.ex
@@ -35,9 +35,10 @@ defmodule AtsWeb.Schema do
     field :jobs, list_of(non_null(:job)), resolve: dataloader(Applicants)
   end
 
+  @desc "A geographic point"
   object :point do
-    field :longitude, :float
-    field :latitude, :float
+    field :longitude, non_null(:float)
+    field :latitude, non_null(:float)
   end
 
   object :job do
@@ -55,19 +56,32 @@ defmodule AtsWeb.Schema do
   end
 
   query do
+    @desc "List all continents."
     field :continents, list_of(non_null(:continent)) do
       pagination()
       resolve(&Resolvers.Continents.list_continents/3)
     end
 
+    @desc "List all professions."
     field :professions, list_of(non_null(:profession)) do
       pagination()
       resolve(&Resolvers.Professions.list_professions/3)
     end
 
+    @desc "List all jobs."
     field :jobs, list_of(non_null(:job)) do
       pagination()
       resolve(&Resolvers.Jobs.list_jobs/3)
+    end
+
+    @desc "Create a point to make geographical queries."
+    field :point, :point do
+      arg(:longitude, non_null(:float))
+      arg(:latitude, non_null(:float))
+
+      resolve(fn _, args, _ ->
+        {:ok, args}
+      end)
     end
   end
 end
